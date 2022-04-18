16af3b7950f9fc066c4ba1942c2c2958f747ce9b
Donald Wu <yeukfei02@gmail.com>
2022-04-18 17:05:22 +0800

    feat: 🎸 add more api

---
+++ api/busStop.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const { getRootUrl } = require("../helpers/helpers");
+
+const rootUrl = getRootUrl();
+
+module.exports.busStopRequest = async (stopId) => {
+  let busStop = {};
+
+  if (stopId) {
+    const response = await axios.get(`${rootUrl}/stop/${stopId}`);
+    if (response) {
+      const responseData = response.data;
+      console.log("responseData = ", responseData);
+
+      if (responseData) {
+        busStop = responseData.data;
+      }
+    }
+  }
+
+  return busStop;
+};
